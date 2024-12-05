d3e23d48a513516412306ea5540acc02a3fe65e1
Racmat Hafidz Fadli <rac.hafidz@gmail.com>
2024-12-05 23:22:30 +0700

    [feat] add loading component

---
+++ src/components/loading/index.tsx
@@ -0,0 +1,9 @@
+import { Spin } from "antd";
+
+export default function Loading() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <Spin size="large" />
+    </div>
+  );
+}
