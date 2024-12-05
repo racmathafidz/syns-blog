02e91796a24e1e4bfbd389721520bb043061bfe0
Racmat Hafidz Fadli <rac.hafidz@gmail.com>
2024-12-05 23:14:06 +0700

    [feat] add author & post type

---
+++ src/types/index.ts
@@ -0,0 +1,15 @@
+export interface Author {
+  id: number;
+  name: string;
+  email: string;
+  gender: string;
+  status: string;
+}
+
+export interface Post {
+  id: number;
+  user_id: number;
+  title: string;
+  body: string;
+  author?: Author;
+}
