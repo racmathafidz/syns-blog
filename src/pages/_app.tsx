dcad6521dc8905913b2c3f1c25991f05a8345c59
Racmat Hafidz Fadli <rac.hafidz@gmail.com>
2024-12-05 23:30:05 +0700

    [feat] add provider and layout

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,20 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import { ConfigProvider } from "antd";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import "@/styles/globals.css";
+import APP_THEME from "@/constants/theme";
+import AppLayout from "@/layout";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const queryClient = new QueryClient();
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ConfigProvider theme={APP_THEME}>
+        <AppLayout>
+          <Component {...pageProps} />
+        </AppLayout>
+      </ConfigProvider>
+    </QueryClientProvider>
+  );
 }
