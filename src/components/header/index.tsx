f37d944dcbc4689983073f5b7a222df88dab2d68
Racmat Hafidz Fadli <rac.hafidz@gmail.com>
2024-12-06 18:47:35 +0700

    [fix] delete profile button

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -3,14 +3,10 @@ import { Layout, Button } from "antd";
 import Link from "next/link";
 import SearchInput from "./searchInput";
 import FilterPopover from "./filterPopover";
-import { getFromLocalStorage } from "@/lib/helper";
-import constants from "@/constants";
 
 const { Header } = Layout;
 
 export default function AppHeader() {
-  const userId = getFromLocalStorage(constants.localStorage.USER_ID);
-
   return (
     <Header className="justify-between">
       <Link href="/">
@@ -22,19 +18,12 @@ export default function AppHeader() {
         <SearchInput />
         <FilterPopover />
       </div>
-      <div className="flex items-center gap-2">
-        <Link href="/create">
-          <Button>
-            <EditOutlined />
-            Write
-          </Button>
-        </Link>
-        <Link href={`/user/${userId}`}>
-          <Button shape="circle">
-            <UserOutlined />
-          </Button>
-        </Link>
-      </div>
+      <Link href="/create">
+        <Button>
+          <EditOutlined />
+          Write
+        </Button>
+      </Link>
     </Header>
   );
 }
