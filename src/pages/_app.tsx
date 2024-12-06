d062307596dd0f585e3ab11ea17748ec4d984b37
Racmat Hafidz Fadli <rac.hafidz@gmail.com>
2024-12-06 18:57:23 +0700

    [refactor] refactoring query client

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,13 @@
 import type { AppProps } from "next/app";
 import { ConfigProvider } from "antd";
-import { QueryClientProvider } from "@tanstack/react-query";
-import { useState } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import APP_THEME from "@/constants/theme";
 import AppLayout from "@/layout";
-import createQueryClient from "@/lib/queryClient";
 import WelcomeDialog from "@/components/welcomeDialog";
 import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
-  const [queryClient] = useState(createQueryClient);
+  const queryClient = new QueryClient();
 
   return (
     <QueryClientProvider client={queryClient}>
