24937baafd5dcffa5a3045b1324d81557ef87cf8
Racmat Hafidz Fadli <rac.hafidz@gmail.com>
2024-12-06 18:37:30 +0700

    [refactor] remove ssr

--- src/pages/blogs/[slug].tsx
+++ src/pages/blogs/[slug].tsx
@@ -1,28 +1,17 @@
 import { useQuery } from "@tanstack/react-query";
-import { Col, Space, Typography } from "antd";
+import { Col, Typography } from "antd";
 import { useRouter } from "next/router";
 import { getDetailPost } from "@/api/posts";
 import Skeleton from "@/components/skeleton";
 import Error from "@/components/error";
 import AuthorInformation from "@/components/users/authorInformation";
-import { GetServerSidePropsContext } from "next";
 import { Author, Post } from "@/types";
-import { getUser } from "@/api/users";
 
 const { Title, Text } = Typography;
 
 interface BlogsProps {
   postData: Post;
   userData: Author;
-}
-
-export async function getServerSideProps(context: GetServerSidePropsContext) {
-  const { slug } = context.params || {};
-  const postData = await getDetailPost(slug);
-  const userData = await getUser(postData?.user_id);
-  return {
-    props: { postData, userData },
-  };
 }
 
 export default function Blogs(props: BlogsProps) {
@@ -32,15 +21,15 @@ export default function Blogs(props: BlogsProps) {
   const {
     data: postData,
     isLoading: isPostDataLoading,
+    isPending: isPostDataPending,
     isError: isPostDataError,
     error: postDataError,
   } = useQuery({
     queryKey: ["posts", slug],
     queryFn: () => getDetailPost(slug),
-    initialData: props.postData,
   });
 
-  if (isPostDataLoading) return <Skeleton />;
+  if (isPostDataLoading || isPostDataPending) return <Skeleton />;
   if (isPostDataError || !postData)
     return <Error message={postDataError?.message} />;
 
@@ -52,9 +41,9 @@ export default function Blogs(props: BlogsProps) {
           user_data={props.userData}
           user_id={postData?.user_id}
         />
-        <Space direction="vertical">
+        <div className="block">
           <Text>{postData?.body}</Text>
-        </Space>
+        </div>
       </Col>
     </div>
   );
